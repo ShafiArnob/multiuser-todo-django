83677c0f509c17f6b1f7b928db1b3907326f09b8
Ahmed Shafi Arnob <46849793+ShafiArnob@users.noreply.github.com>
2021-07-08 02:39:32 +0600

    loogin

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    path(),
+    path('', views.home, name='home'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+# Home page
+def home(request):
+  return render(request, 'index.html')
+
+# Login Page
+def login(request):
+  return render(request, 'login.html')
+
+# Signup Page
+def signup(request):
+  if request.method == "POST":
+    fm = UserCreationForm(request.POST)
+    if fm.is_valid():
+      messages.success(request,"Account Created!!")
+      fm.save()
+      #print("user: " , user)
+      return redirect("login")
+  else:
+    fm = UserCreationForm()
+  return render(request,'signup.html',{'form':fm})
